da81c9b4186e12dfc1f95fbcceb73c64c18ce364
Aurelien <a.adnot@gmail.com>
2019-10-09 23:01:48 +0200

    AQUA - requete AJAX to get /posts from the api

---
+++ src/components/aquaGate.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-layout>
+     Start a new blog 
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data: () => ({
+  }),
+  created(){
+      axios.get('http://localhost:80/admin-system/api/posts')
+      .then((response)=>{
+        console.log(response.data);
+      })
+      .catch((err)=>{
+        console.log('error', err);
+      });
+  methods:{
+    
+    }
+  }
+};
+</script>
