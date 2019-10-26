04ba340a60022e54ca873b30bc3f1d43ca9d2e60
Aurelien <a.adnot@gmail.com>
2019-10-26 23:38:42 +0200

    AQUA - vuex-persistedstate put in place

--- src/store/module.js
+++ src/store/module.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,7 @@ import {mutations} from './mutations';
 import {actions} from './actions';
 
 export default new Vuex.Store({
+    plugins: [createPersistedState()],
     namespaced: true,
     state,
     getters,
