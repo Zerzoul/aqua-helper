566e5d29ff9c533b897a4ce0e6dd9072e3ed6299
Aurelien <a.adnot@gmail.com>
2019-11-06 21:24:52 +0100

    AQUA - change the page setting  of contentArticleBillet

--- src/components/content/blog/contentArticleBillet.vue
+++ src/components/content/blog/contentArticleBillet.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <v-img class="white--text align-end" height="370px" :src="getImage" :min-width="`${100}%`">
-      <v-card-title class="accent" >
-       <v-row class="px-5" align="center" justify="space-between">
-         <span class="display-1">{{ billet.title }}</span>
-         <span class="caption">{{ billet.date_create }}</span>
-       </v-row>
+      <v-card-title class="accent">
+        <v-container pa-0>
+        <v-row class="px-5" align="center" justify="space-between">
+          <span class="display-1">{{ billet.title }}</span>
+          <span class="caption">{{ billet.date_create }}</span>
+        </v-row>
+        </v-container>
       </v-card-title>
     </v-img>
-    <v-card-text>
-      <div>
-        <span v-html="billet.content"></span>
-      </div>
-    </v-card-text>
+    <v-container>
+      <v-card-text>
+        <div>
+          <span v-html="billet.content"></span>
+        </div>
+      </v-card-text>
+    </v-container>
   </div>
 </template>
 
